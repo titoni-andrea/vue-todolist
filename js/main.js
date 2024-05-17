8159bf328a4d081dc3394af8d337fff91d920984
titoni-andrea <andreatitoni@gmail.com>
2024-05-17 16:10:43 +0200

    m0 - stampare in pagina array stringhe

--- js/main.js
+++ js/main.js
@@ -3,13 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-           
+           todos: [
+            "Pippo",
+            "Mauro",
+            "Giovanni",
+            "Sandro",
+            "Anna",
+            "Sara",
+           ]
         }
     },
     methods: {
 
     },
-    
+
     mounted() {
     },
 }).mount(`#app`)
